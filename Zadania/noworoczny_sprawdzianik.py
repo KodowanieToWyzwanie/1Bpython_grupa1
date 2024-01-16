4a01d9635ce72e83169fd16930e4b01a5a3fa86b
KodowanieToWyzwanie <148467446+KodowanieToWyzwanie@users.noreply.github.com>
2024-01-16 09:31:08 +0100

    Update noworoczny_sprawdzianik.py
    solution of the first 6 tasks

--- Zadania/noworoczny_sprawdzianik.py
+++ Zadania/noworoczny_sprawdzianik.py
@@ -1,9 +1,52 @@
-# Pobierz z ekranu swoje imię i wyświetl napis- “Twoje imię” - super, że jesteś
+#pobierz z ekranu swoje imię i wyświetl napis- "Twoje imię" - super, że jesteś
+
+name = input("podaj swoje imię: ")
+print(name + " - super, że jesteś!")
+
+
 # Właśnie zakładasz stronę z treściami tylko dla dorosłych. Wyświetl na ekranie stosowny komunikat i spytaj się użytkownika o wiek - jeśli ma poniżej 18, w grzeczny sposób odmów dostępu do strony, w przeciwnym razie napisz, że użytkownik może korzystać ze strony.
+
+wiek = int(input("Podaj swój wiek: "))
+if wiek >=0 and wiek <=18:
+  print("Przykro nam, ale ta strona jest tylko dla dorosłych ")
+elif wiek >=18 and wiek <=120:
+  print("Możesz korzystać ze strony ")
+else:
+  print("Nieprawidłowa wartość")
+
+
 # Oblicz BMI - dla podanych przez użytkownika wartości wagi i wzrostu -należy podzielić wagę wyrażoną w kilogramach przez wzrost podniesiony do kwadratu, wyrażony w metrach, czyli w skrócie BMI = kg / m2.
+
+
+waga = float(input("Podaj swoją wagę w kilogramach: "))
+wzrost = float(input("Podaj swój wzrost w metrach: "))
+bmi = waga / (wzrost **2)
+print("Twoje bmi wynosi:", bmi)
+
+
 # Zdrowy dorosły człowiek o prawidłowym stosunku wagi do wzrostu ma wskaźnik w granicach 18,5–24,9. BMI poniżej 18,5 informuje o niedowadze. Osoby ze wskaźnikiem BMI 25,0-29,9 cierpią na nadwagę. Współczynnik BMI powyżej 30,0 to już otyłość.Wyświetl odpowiedni komunikat do uzyskanego wyniku. 
+
+if bmi < 18.5:
+  print("Niedowaga")
+elif bmi >= 18.5 and bmi <= 24.9:
+  print("Prawidłowa waga")
+elif bmi >= 25.0 and bmi <= 29.9:
+  print("Nadwaga")
+else:
+  print("Otyłość")
+
 # Wyświetl na ekranie liczby nieparzyste od 10 do 10 (włącznie) <10,20> w pętli for lub while
-# Jesteś rekruterem stewardess do linii lotniczych. Chcesz by wszystkie dziewczyny w Twoim zespole miały wzrost od <167-180> . Sprawdź czy podane dane przez użytkownika spełniają ten warunek.
+
+for i in range(11, 21, 2):
+  print(i)
+
+#Jesteś rekruterem stewardess do linii lotniczych. Chcesz by wszystkie dziewczyny w Twoim zespole miały wzrost od <167-180> . Sprawdź czy podane dane przez użytkownika spełniają ten warunek.
+
+wzrost = int(input("Podaj wzrost w cm: "))
+if wzrost >= 167 and wzrost <= 180:
+  print("Wzrost spełnia warunek")
+else:
+  print("Wzrost nie spełnia warunku")
 
 # Wyświetl na ekranie liczby parzyste od 0 do 10 (włącznie) <0,10> w pętli for lub while
 # Jesteś geografem i chcesz przy pomocy komputera w szybki sposób policzyć jaka jest różnica między najwyższym a najniższym miejscem w podanym kraj. Najpierw policz jaka jest różnica w Polsce. depresja Marzęcino -2.2 Rysy 2499. Użytkownik podaje dwie liczby. Dla ułatwienia obliczeń najpierw podaje liczbę mniejszą(bądź też ujemną)
