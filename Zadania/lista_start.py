c232adec58e0ddd2cc8c7f7fe7e13381e188c004
KodowanieToWyzwanie <148467446+KodowanieToWyzwanie@users.noreply.github.com>
2024-01-19 14:13:06 +0100

    Create lista_start.py
    practise list in python

---
+++ Zadania/lista_start.py
@@ -0,0 +1,21 @@
+https://kt.academy/pl/article/py-listy
+
+Jak mocno się śmiejesz? Pobierz liczbę od użytkownika i wydrukuj słowo “Ha” tyle razy ile podał użytkownik
+Wpisz z ekranu swoje imię i podaj jego długość 
+Wpisz z ekranu swoje imię - i wypisz wpisane od tyłu
+Wpisz z ekranu napis i sprawdź czy podany napis od tyłu jest taki sam - dla “kajak” wypisz “tak, od tyłu”  dla “Kubek” - “nie….” 
+Popraw program z zadania 4 tak aby działał niezależnie od podanych przez użytkownika wielkości liter
+Utwórz pustą tablicę
+Utwórz tablicę z ulubionymi kolegami z Twojej klasy
+Oblicz ile jest elementów tablicy z zadania 7
+Utwórz tablicę z liczbami od 1 do 10 w pętli
+Utwórz tablicę z twoimi czterema ulubionymi potrawami w pętli (while/for)
+Utwórz tablicę z twoimi ocenami - w pętli while - za każdym razem się pytając czy to ostatnia ocena 
+Utwórz tablicę 10 elementową z liczbami losowymi z zakresu od 1,10
+Utwórz tablicę 20 elementową z liczbami losowymi z zakresu od 1,50
+Utwórz tablicę 3 elementową z liczbami losowymi z zakresu od (0,1) zaokrąglij liczbę do 2 miejsc po przecinku
+Utwórz tablicę z dzielnikami liczby podanej przez użytkownika (łącznie z 1 i podaną liczbą)
+Utwórz tablicę z dzielnikami liczby podanej przez użytkownika (łącznie z 1 i podaną liczbą) - sprawdź czy długość listy jest równa 2 - jeśli tak wydrukuj “Liczba pierwsza”, w przeciwnym wypisz “Dzielniki {liczby} to {wypisz dzielniki}”, zadbaj o właściwy komunikat gdy użytkownik poda 0 lub 1 
+W pętli true, pytając się czy chcesz zakończyć - pobierz od użytkownika liczbę - jeśli jest pierwsza dodaj do tablicy, na końcu działania programu wypisz podane przez użytkownika liczby pierwsze
+Dla podanych dwóch liczb przez użytkownika, podaj największy wspólny dzielnik, największą wspólną wielokrotność
+Podaj rozkład na czynniki pierwsze dla podanej przez użytkownika liczby, za każdym razem wynik dodając do listy
