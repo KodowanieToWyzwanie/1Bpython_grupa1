9fc36e782338c80ccbbe01220eac41416508c5ca
KodowanieToWyzwanie <148467446+KodowanieToWyzwanie@users.noreply.github.com>
2024-01-19 14:08:33 +0100

    Create 19_01_2024.py
    rozkład na czynniki
    liczba pierwsza

---
+++ ZLekcji/19_01_2024.py
@@ -0,0 +1,54 @@
+tab_imion= ["jan05534523","jan1","jan2","jan3","jan4"]
+# print(tab_imion[2])
+# for i in range(len(tab_imion)):
+#   print(tab_imion[i])
+a = tab_imion[0]
+print(a[::-1])
+
+#rozkład na czynniki pierwsze
+# liczba = 8
+# dzielnik = 2
+# while dzielnik <= liczba:
+#   while liczba%dzielnik == 0:
+#     liczba //= dzielnik
+#     print(dzielnik,end=" ")
+#   dzielnik +=1
+##największy wspólny dzielnik
+# a = 30
+# b = 10
+# a1, b1 = a, b
+# while a != b:
+#   if a > b:
+#     a = a - b
+#   else:
+#     b = b - a
+    
+# nww = a1 * b1 / a
+
+
+# while b != 0:
+#   a, b = b, a % b
+# print(a)
+
+# imie ="Katarzyna"
+# print(len(imie))
+
+# #czy pierwsza
+# liczba = 7
+# for i in range(2, (liczba // 2) + 1):
+#   if liczba % i == 0:
+#     print("zlożona")
+#     break
+# else:
+#   print("pierwsza")
+
+# imiona = []
+# liczba_imion = int(input("Podaj liczbę imion\n"))
+# kontynuacja = input("czy kontunuować\n").lower()
+# print(kontynuacja)
+# # while liczba_imion > 0:
+# #   liczba_imion -=1
+# for _i in range(liczba_imion):
+#   imiona.append(input("podaj imie\n"))
+
+# print(imiona)
